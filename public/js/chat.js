209a0810ea640a520265dfa4138249b04d5fd808
Shivam24S <shivamshrivastav8055@gmail.com>
2024-09-03 12:33:55 +0530

    form and location button state (enabling and disabling )

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,12 +12,22 @@ socket.on("message", (msg) => {
   console.log(msg);
 });
 
-document.querySelector("#msg-form").addEventListener("submit", (e) => {
+// elements
+
+const $messageForm = document.querySelector("#msg-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const message = e.target.elements.message.value;
 
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled", "disabled");
+    ($messageFormInput.value = ""), $messageFormInput.focus();
     if (error) {
       return console.log("error", error);
     }
@@ -25,18 +35,23 @@ document.querySelector("#msg-form").addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#location").addEventListener("click", () => {
+const $locationButton = document.querySelector("#location");
+
+$locationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     alert("your browser does not support geolocation");
   }
-
+  $locationButton.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition((position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     // console.log("latitude " + " " + lat, "longitude " + " " + lon);
 
+    $locationButton.setAttribute("disabled", "disabled");
+
     socket.emit("shareLocation", lat, lon, (serverMsg) => {
       console.log("location was captured!", "serverMsg:", serverMsg);
     });
+    $locationButton.removeAttribute("disabled", "disabled");
   });
 });
